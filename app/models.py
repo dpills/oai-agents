49cddc592fdfb1e360760e160d08683da4368446
dpills <dpills@protonmail.com>
2025-03-12 14:36:11 -0400

    Remove unused models

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Todo(BaseModel):
-    id: str = Field(title="Unique identifier of the todo task")
-    title: str = Field(title="Title of the todo task")
-    completed: bool = Field(title="Indicates if the todo is completed")
