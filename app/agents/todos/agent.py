9102c96a21bf35a60f96fb0e18c16eb0e551b7e6
dpills <dpills@protonmail.com>
2025-03-12 13:42:24 -0400

    inline

--- app/agents/todos/agent.py
+++ app/agents/todos/agent.py
@@ -2,18 +2,10 @@ from agents import Agent
 
 from .tools import todo_tools
 
-handoff_description = """\
-Specialist agent for creating, updating, deleting and getting todo tasks"
-"""
-
-instructions = """\
-You help users manage their todos tasks.
-"""
-
 todo_agent = Agent(
     model="gpt-4o",
     name="Todo manager",
-    handoff_description=handoff_description,
-    instructions=instructions,
+    handoff_description="Specialist agent for creating, updating, deleting and getting todo tasks",  # noqa: E501
+    instructions="You help users manage their todos tasks.",
     tools=todo_tools,
 )
