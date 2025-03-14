9220b8fdd89c4548f9790e74400b39cb8da62ae4
dpills <dpills@protonmail.com>
2025-03-14 10:42:30 -0400

    Stream tool progress and results

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import asyncio
+from typing import Any
 
+import orjson
 from agents import Runner, set_default_openai_client, trace
-from openai.types.chat import (
-    ChatCompletionAssistantMessageParam,
-    ChatCompletionUserMessageParam,
+from openai.types.responses import (
+    EasyInputMessageParam,
+    ResponseFunctionToolCall,
+    ResponseInputItemParam,
+    ResponseOutputItemAddedEvent,
+    ResponseTextDeltaEvent,
 )
-from openai.types.responses import ResponseFunctionToolCallParam, ResponseTextDeltaEvent
-from openai.types.responses.response_input_item_param import FunctionCallOutput
 
 from .agents import supervisor_agent
 from .clients import openai_client
@@ -18,63 +21,70 @@ async def main() -> None:
     """
     OpenAI Agents
     """
-    messages: list[
-        ChatCompletionUserMessageParam
-        | ChatCompletionAssistantMessageParam
-        | ResponseFunctionToolCallParam
-        | FunctionCallOutput
-    ] = []
+    convo_msgs: list[ResponseInputItemParam] = []
+    tool_calls: dict[str, Any] = {}
 
     with trace(workflow_name="Conversation", group_id="1"):
         while True:
-            messages.append(
-                ChatCompletionUserMessageParam(role="user", content=input("📝: "))
+            user_input = input("📝: ").strip()
+            if not user_input or user_input.lower() == "q":
+                break
+
+            result = Runner.run_streamed(
+                starting_agent=supervisor_agent,
+                input=[
+                    *convo_msgs,
+                    EasyInputMessageParam(role="user", content=user_input),
+                ],
+                max_turns=20,
             )
 
-            result = Runner.run_streamed(supervisor_agent, messages, max_turns=20)
-
-            assistant_msg = ""
             async for event in result.stream_events():
                 if event.type == "raw_response_event" and isinstance(
                     event.data, ResponseTextDeltaEvent
                 ):
-                    assistant_msg += event.data.delta
                     print(event.data.delta, end="", flush=True)
+                elif (
+                    event.type == "raw_response_event"
+                    and isinstance(event.data, ResponseOutputItemAddedEvent)
+                    and isinstance(event.data.item, ResponseFunctionToolCall)
+                    and not event.data.item.name.startswith("transfer_to_")
+                ):
+                    print(f"TOOL CALL: {event.data.item.name}")
                 elif (
                     event.type == "run_item_stream_event"
                     and event.name == "tool_called"
                 ):
-                    messages.append(
-                        ResponseFunctionToolCallParam(
-                            type="function_call",
-                            id=event.item.raw_item.id,
-                            call_id=event.item.raw_item.call_id,
-                            name=event.item.raw_item.name,
-                            arguments=event.item.raw_item.arguments,
-                            status="completed",
-                        )
-                    )
+                    args = None
+                    if event.item.raw_item.arguments:
+                        args = orjson.loads(event.item.raw_item.arguments)
+
+                    tool_calls[event.item.raw_item.call_id] = {
+                        "name": event.item.raw_item.name,
+                        "arguments": args,
+                    }
                 elif (
                     event.type == "run_item_stream_event"
                     and event.name == "tool_output"
                 ):
-                    messages.append(
-                        FunctionCallOutput(
-                            type="function_call_output",
-                            call_id=event.item.raw_item["call_id"],
-                            output=event.item.raw_item["output"],
-                        )
-                    )
+                    call_id = event.item.raw_item["call_id"]
+                    if call_id and isinstance(call_id, str):
+                        tool_call = tool_calls.get(call_id)
+                        if tool_call:
+                            if tool_call["arguments"]:
+                                print(
+                                    f"TOOL: {tool_call['name']} - {tool_call['arguments']} \n\n {event.item.raw_item['output']}"  # noqa: E501
+                                )
+                            else:
+                                print(
+                                    f"TOOL: {tool_call['name']} \n\n {event.item.raw_item['output']}"  # noqa: E501
+                                )
+                        else:
+                            print(f"TOOL: {event.item.raw_item['output']}")
 
             print()
 
-            messages.append(
-                ChatCompletionAssistantMessageParam(
-                    role="assistant", content=assistant_msg
-                )
-            )
-
-            print()
+            convo_msgs = result.to_input_list()
 
 
 if __name__ == "__main__":
