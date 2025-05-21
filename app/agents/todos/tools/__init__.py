353544c5491166617ea212457c8c7eba3d327a94
depillsb <depillsb@cisco.com>
2025-05-21 08:42:21 -0400

    Fix type

--- app/agents/todos/tools/__init__.py
+++ app/agents/todos/tools/__init__.py
@@ -1,5 +1,12 @@
+from agents import ComputerTool, FileSearchTool, FunctionTool, WebSearchTool
+
 from .tools import add_todo, delete_todo, get_todos, update_todo
 
-todo_tools = [add_todo, delete_todo, get_todos, update_todo]
+todo_tools: list[FunctionTool | FileSearchTool | WebSearchTool | ComputerTool] = [
+    add_todo,
+    delete_todo,
+    get_todos,
+    update_todo,
+]
 
 __all__ = ("todo_tools",)
